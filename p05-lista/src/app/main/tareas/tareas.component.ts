b9dff7fd705b943fa8206801c2013a70c7c3f908
jargomez <ja.ruizgomez@gmail.com>
2017-11-29 10:25:08 +0100

    Lista - localStorage y uso de template en angular

--- p05-lista/src/app/main/tareas/tareas.component.ts
+++ p05-lista/src/app/main/tareas/tareas.component.ts
@@ -12,15 +12,30 @@ export class TareasComponent implements OnInit {
 
 
   constructor() {
-    this.aTareas = [];
   }
 
+  // localStorage es de HTML5 para almacenar datos; parecido a las cookies
   ngOnInit() {
+    if (localStorage.getItem('tareas')) {
+      this.aTareas = JSON.parse(localStorage.getItem('tareas'));
+    } else {
+      this.aTareas = [];
+    }
   }
 
   addTareas() {
     this.aTareas.push(this.sTarea);
+    this.actualizarStorage();
     this.sTarea = '';
+  }
+
+  deleteTarea(i: number) {
+    this.aTareas.splice(i, 1);
+    this.actualizarStorage();
+  }
+
+  private actualizarStorage() {
+    localStorage.setItem('tareas', JSON.stringify(this.aTareas));
   }
 
 }
