10256a95a0c86949a8c8b2699ef3627836e4ee48
jargomez <ja.ruizgomez@gmail.com>
2017-11-30 14:20:09 +0100

    Enrutamiento perezoso - carga por módulos

---
+++ p09-lazzy/src/app/shared/cabeza/cabeza.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabeza',
+  templateUrl: './cabeza.component.html',
+  styleUrls: ['./cabeza.component.css']
+})
+export class CabezaComponent implements OnInit {
+  // controlador del componente
+
+  title: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.title = 'Rutas en Angular';
+  }
+
+}
---
+++ p09-lazzy/src/app/shared/logo/logo.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  template: `
+    <img class="img-fluid" [alt]="altImg" [src]="logoImg">
+  `
+})
+export class LogoComponent implements OnInit {
+
+  logoImg: string;
+  altImg: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.logoImg = '../../../assets/logo_angular.svg';
+    this.altImg = 'Angular Logo';
+  }
+
+}
